9582041d10e4592427c4f4e9b87923c259dca43e
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-09 07:20:14 +0800

    making sure the images are added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getFetch(){
+    const url = `https://api.nasa.gov/planetary/apod?api_key=5bv4pmQ9vZOmoYLVVmfUMJQs1tiIxxHOa0xtsjGB`
+  
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data)
+          if(data.media_type === 'image') {
+            document.querySelector('.nasaImage').src = data.hdurl
+          } else if(data.media_type === 'video') {
+            document.querySelector('iframe').src = data.url
+          }
+          
+          document.querySelector('h3').innerText=data.explanation
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  }
+  
+  getFetch()
