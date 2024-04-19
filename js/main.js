1fe6d17f88463871e06ad6489ef3fea8536e688f
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-19 23:24:03 +0800

    added music files and started to prepare radio properties

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 //document.querySelector('button').addEventListener('click', getFetch)
 
+
+function playMusic() {
+  const audio1 = new Audio("music/Confuso.mp3")
+  const audio2 = new Audio("music/Lovely.mp3")
+  const audio3 = new Audio("music/Star Guitar.mp3")
+
+  let stationNumber = Math.floor(Math.random() * 3) + 1
+  
+  switch (stationNumber) {
+    case 1:
+      audio1.play();
+      break;
+    case 2:
+      audio2.play();
+      break;
+    case 3:
+      audio3.play();
+      break;
+
+  }
+ 
+}
+
+
+
 function getFetch(){
   const url = `https://api.nasa.gov/planetary/apod?api_key=5bv4pmQ9vZOmoYLVVmfUMJQs1tiIxxHOa0xtsjGB`
 
